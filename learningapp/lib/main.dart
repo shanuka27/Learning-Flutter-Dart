03cf0c473de4efbef31bf1351226ea8a8ecad804
shanuka27 <shanukayasiru27@gmail.com>
2023-08-22 12:55:37 +0530

    column, row widget

---
+++ learningapp/lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: SafeArea(
+         child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 100,
+                decoration:BoxDecoration( 
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                 )
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 180,
+                    height: 100,
+                    decoration:BoxDecoration( 
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(10),
+                     )
+                  ),
+                  Container(
+                    width: 180,
+                    height: 100,
+                    decoration:BoxDecoration( 
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(10),
+                     )
+                  ),
+              ],
+              ),
+              Container(
+                width: double.infinity,
+                height: 100,
+                decoration:BoxDecoration( 
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                 )
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 130,
+                    height: 100,
+                    decoration:BoxDecoration( 
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(10),
+                     )
+                  ),
+                  Container(
+                    width: 130,
+                    height: 100,
+                    decoration:BoxDecoration( 
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(10),
+                     )
+                  ),
+                  Container(
+                    width: 130,
+                    height: 100,
+                    decoration:BoxDecoration( 
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(10),
+                     )
+                  ),
+              ],
+              ),
+
+                Container(
+                width: double.infinity,
+                height: 100,
+                decoration:BoxDecoration( 
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                 )
+              ),
+
+            ],
+          )
+         )
+        ),
+      );
+  }
+}
+
